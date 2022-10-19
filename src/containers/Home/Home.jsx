d723c7045ba685b2c2b5bd646e3fc2bb122ca0c4
Gavin Stirling <gavinstir@gmail.com>
2022-10-19 15:05:25 +0100

    Created footer

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import Navigation from '../../components/Navigation/Navigation'
-import Intro from '../../components/Intro/Intro'
+import Intro from '../Intro/Intro'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navigation />
         <Intro />
+        <Footer />
     </div>
   )
 }
